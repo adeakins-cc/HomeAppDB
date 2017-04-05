3ee31924d41431f0b50a4990828ff532cc6ece32
adeakins-cc <Adeakins-cc@conestogac.on.ca>
2017-04-05 13:29:22 -0400

    New DB

---
+++ HomeAppDB/Customer.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[CustomerId] INT NOT NULL PRIMARY KEY DEFAULT 0, 
+    [companyName] VARCHAR(50) NULL, 
+    [firstName] VARCHAR(20) NOT NULL, 
+    [lastName] VARCHAR(20) NOT NULL, 
+    [contactName] VARCHAR(20) NOT NULL, 
+    [contactPhone] INT NULL, 
+    [contactMobile] INT NULL, 
+    [fax] INT NULL, 
+    [bussinessPhone] INT NULL, 
+    [contactEmail] VARCHAR(50) NULL, 
+    [unitNumber] INT NULL, 
+    [streetNumber] INT NOT NULL, 
+    [streetName] VARCHAR(50) NOT NULL, 
+    [city] VARCHAR(20) NOT NULL, 
+    [province] VARCHAR(2) NOT NULL DEFAULT 'ON', 
+    [country] VARCHAR(20) NOT NULL DEFAULT 'Canada', 
+    [postalCode] VARCHAR(10) NOT NULL
+)
---
+++ HomeAppDB/Property.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Property]
+(
+	[PropertyId] INT NOT NULL PRIMARY KEY DEFAULT 0, 
+    [CustomerId] INT NOT NULL DEFAULT 0, 
+    [unitNumber] INT NULL DEFAULT 0 , 
+    [streetNumber] INT NOT NULL DEFAULT 0, 
+    [streetName] VARCHAR(50) NOT NULL, 
+    [city] VARCHAR(20) NOT NULL, 
+    [province] VARCHAR(2) NOT NULL, 
+    [country] VARCHAR(20) NOT NULL, 
+    [postalCode] VARCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_CustomerId] FOREIGN KEY (CustomerId) REFERENCES [Customer](CustomerId)
+)
