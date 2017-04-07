1ee7397585552893291401cf7c4a141804a7bed0
adeakins-cc <Adeakins-cc@conestogac.on.ca>
2017-04-07 17:46:04 -0400

    update
    Reorganize tables

--- HomeAppDB/Scripts/PartList.sql
+++ HomeAppDB/Scripts/PartList.sql
@@ -1,11 +1,11 @@
-﻿CREATE TABLE [dbo].[PartsUsed]
+﻿CREATE TABLE [dbo].[PartsList]
 (
 	[partListId] INT NOT NULL PRIMARY KEY , 
     [invoiceId] INT NOT NULL DEFAULT 0, 
     [partId] INT NOT NULL DEFAULT 0, 
     [editPrice] DECIMAL(19, 2) NOT NULL DEFAULT 0, 
     [quantity] INT NOT NULL DEFAULT 0, 
-    [categoryName] VARCHAR(100) NOT NULL DEFAULT '', 
+    [partName] VARCHAR(100) NOT NULL DEFAULT '', 
     CONSTRAINT [FK_PartList_Invoice] FOREIGN KEY (invoiceId) REFERENCES [Invoice](invoiceId), 
     CONSTRAINT [FK_PartList_Part] FOREIGN KEY (partId) REFERENCES [Part](partId)
 )
