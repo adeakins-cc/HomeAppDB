8fde421e7c5151e44ed86c30151694696272a82b
adeakins-cc <adeakins-cc@conestogac.on.ca>
2017-04-09 21:17:25 -0400

    Update DB for migration
    easing ability to migrate old db

--- HomeAppDB/Scripts/Finacial.sql
+++ HomeAppDB/Scripts/Finacial.sql
@@ -1,14 +1,15 @@
 ﻿CREATE TABLE [dbo].[Finacial]
 (
 	[finacialId] INT NOT NULL PRIMARY KEY , 
-    [invoiceId] INT NOT NULL DEFAULT 0, 
-    [postDate] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [postDate] DATETIME NOT NULL DEFAULT GETDATE(),
+    [invoiceId] INT NOT NULL DEFAULT 0,  
     [totalDue] DECIMAL(19, 2) NOT NULL DEFAULT 0, 
     [amountPaid] DECIMAL(19, 2) NULL DEFAULT 0, 
-    [chequeNumber] VARCHAR(50) NULL DEFAULT '', 
+	[notes] VARCHAR(250) NULL DEFAULT '',
+	[paidDate] DATETIME NULL DEFAULT GETDATE(),
 	[chequeName] VARCHAR(50) NULL DEFAULT '',
-    [paidDate] DATETIME NULL DEFAULT GETDATE(), 
+    [chequeNumber] VARCHAR(50) NULL DEFAULT '',
     [dateOnCheque] DATETIME NULL DEFAULT GETDATE(), 
-    [notes] VARCHAR(250) NULL DEFAULT '',  
+      
     CONSTRAINT [FK_Finance_Invoice] FOREIGN KEY (invoiceId) REFERENCES [Invoice](invoiceId)
 )
